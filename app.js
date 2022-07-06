b4679a7948e4f583bd7f7aadc2483d965b9fe754
Diego Candido <diego@diegocandido.com>
2022-07-05 23:40:50 -0300

    Ajustes

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
 const express = require('express')
-const cors = require('cors');
 const app = express()
+const cors = require('cors');
 const port = process.env.PORT || 8080
 
 const { publicacoes } = require('./models/articles')
 const { catgames } = require('./models/games')
 const { catweb } = require('./models/web')
 
-
 app.use(express.json())
 app.use(express.urlencoded({
     extende: true
 }))
+app.use(cors());
+
 
 app.use('/img', express.static(__dirname + '/public/images'));
 
-app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});
 
 //LISTAR TODAS AS POSTAGENS
 app.get('/postagens', (req,res)=>{
