8d5f152ee137ddf2d00ea1e90675178b37fc50e7
Diego Candido <diego@diegocandido.com>
2023-09-19 20:40:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const cors = require('cors');
+
+app.use(cors())
+
+
 const port = process.env.PORT || 8080
 
 const { publicacoes } = require('./models/articles')
@@ -10,7 +14,6 @@ app.use(express.json())
 app.use(express.urlencoded({
     extende: true
 }))
-app.use(cors());
 
 
 app.use('/img', express.static(__dirname + '/public/images'));
